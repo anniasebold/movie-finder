05e90afb51d7a6c091c0fce69aeb61117bf60e99
anniasebold <anniasebold3.0@gmail.com>
2023-05-26 21:34:20 -0300

    listagem de filmes puxando pela API

--- src/Components/Movie/MovieCard/MovieCard.js
+++ src/Components/Movie/MovieCard/MovieCard.js
@@ -1,97 +1,68 @@
 import './MovieCard.scss';
 import { Card, Container, Row, Col } from 'react-bootstrap';
 import axios from 'axios';
+import React from 'react';
 
-const movies =
-  {
-    "adult": false,
-    "backdrop_path": "/4t0oBFrJyweYPt0hocW6RUa0b6H.jpg",
-    "genre_ids": [
-      28,
-      80,
-      53
-    ],
-    "id": 385687,
-    "original_language": "en",
-    "original_title": "Fast X",
-    "overview": "Ao longo de muitas missões e contra probabilidades impossíveis, Dom Toretto e sua família foram mais espertos, mais nervosos e superaram todos os inimigos em seu caminho. Agora, eles enfrentam o oponente mais letal que já enfrentaram: uma ameaça terrível emergindo das sombras do passado, alimentada por vingança de sangue e determinada a destruir esta família e destruir tudo - e todos - que Dom ama, para sempre. .",
-    "popularity": 4334.616,
-    "poster_path": "/4CWoalqAsRzXD9AFbByD1KnH40E.jpg",
-    "release_date": "2023-05-17",
-    "title": "Velozes & Furiosos 10",
-    "video": false,
-    "vote_average": 6.9,
-    "vote_count": 449
-};
+const API_KEY = 'bf74bdfa989ad758eb544fbbde7650e4'
+const language = 'pt-BR'
 
-const genres = [
-  {
-      "id": 28,
-      "name": "Action"
-  },
-  {
-      "id": 80,
-      "name": "Crime"
-  },
-  {
-      "id": 99,
-      "name": "Documentary"
-  },
-  {
-      "id": 53,
-      "name": "Thriller"
-  }
-]
 function MovieCard() {
+  const [movies, setMovies] = React.useState([]);
+  const [genres, setGenres] = React.useState([]);
+
+  React.useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        const response = await axios.get(
+          `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=${language}`
+        );
+
+        setMovies(response.data.results);
+      } catch(e) {
+        console.error(e);
+      }
+    };
+
+    const fetchGenres = async () => {
+      try {
+        const response = await axios.get(
+          `https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}&language=${language}`
+        );
+          
+        setGenres(response.data.genres);
+      } catch(e) {
+        console.error(e);
+      }
+    }
+
+    fetchMovies();
+    fetchGenres();
+  }, []);
+
+  const getGenresName = (movie) => {
+    return (
+      genres
+        .filter(genre => movie.genre_ids.includes(genre.id))
+        .map(genre => genre.name)
+        .join(', ')
+    )
+  }
+
   return (
     <>
     <Container>
       <Row xs={1} md={2} className="">
-        <Col md={3}>
-          <Card id="movie-card">
-            <Card.Img variant="top" src={`https://image.tmdb.org/t/p/w300/${movies.poster_path}`} />
-            <Card.Body>
-              <Card.Title>{movies.title}</Card.Title>
-              <Card.Text>{movies.genre_ids.join(", ")}</Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col md={3}>
-          <Card id="movie-card">
-            <Card.Img variant="top" src={`https://image.tmdb.org/t/p/w300/${movies.poster_path}`} />
-            <Card.Body>
-              <Card.Title>{movies.title}</Card.Title>
-              <Card.Text>{movies.genre_ids.join(", ")}</Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col md={3}>
-          <Card id="movie-card">
-            <Card.Img variant="top" src={`https://image.tmdb.org/t/p/w300/${movies.poster_path}`} />
-            <Card.Body>
-              <Card.Title>{movies.title}</Card.Title>
-              <Card.Text>{movies.genre_ids.join(", ")}</Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col md={3}>
-          <Card id="movie-card">
-            <Card.Img variant="top" src={`https://image.tmdb.org/t/p/w300/${movies.poster_path}`} />
-            <Card.Body>
-              <Card.Title>{movies.title}</Card.Title>
-              <Card.Text>{movies.genre_ids.join(", ")}</Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col md={3}>
-          <Card id="movie-card">
-            <Card.Img variant="top" src={`https://image.tmdb.org/t/p/w300/${movies.poster_path}`} />
-            <Card.Body>
-              <Card.Title>{movies.title}</Card.Title>
-              <Card.Text>{movies.genre_ids.join(", ")}</Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
+        {movies.map((movie) => (
+          <Col key={movie.id} md={3}>
+            <Card id="movie-card">
+              <Card.Img variant="top" src={`https://image.tmdb.org/t/p/w300/${movie.poster_path}`} />
+              <Card.Body>
+                <Card.Title>{movie.title}</Card.Title>
+                <Card.Text>{getGenresName(movie)}</Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))};
       </Row>
     </Container>
     </>
