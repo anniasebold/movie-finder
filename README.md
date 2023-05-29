ad2b0509f19bd1b9db7eb41beb941890c7a1564c
anniasebold <anniasebold3.0@gmail.com>
2023-05-29 16:41:50 -0300

    Nota com estrelas, atores tela do filme 80% finalizada

--- README.md
+++ README.md
@@ -12,3 +12,28 @@
 
   - Prototipação das telas no Figma: 
     - https://www.figma.com/file/XpUS40eIL0oO3U3cL3mVo2/Movie-Finder?node-id=0%3A1&t=dJrh8jZhkita80zN-1
+  
+  - Implementação das telas do Figma
+    - Nessa parte do projeto, foram escolhidas as tecnologias que seriam utilizadas e implementado as telas seguindo a protipação proposta no Figma.
+
+## 👨‍💻 Como instalar e rodar o projeto
+
+  - Primeiramente clone o repositório
+    
+      
+        git clone https://github.com/anniasebold/movie-finder
+
+  - Instale o Node se ainda não tiver instalado (de preferência uma versão acima da 18).
+  - Entre no repositório que foi clonado
+
+        cd movie-finder
+
+  - Instale as dependências
+
+        npm install
+  
+  - Rode o projeto
+
+        npm start
+    
+      Assim o projeto já estará rodando pronto para o uso.
--- src/Components/Movie/MovieDetails/Movie.js
+++ src/Components/Movie/MovieDetails/Movie.js
@@ -1,9 +1,12 @@
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { Badge, Col, Container, Row } from "react-bootstrap";
+import { Badge, Button, Col, Container, Row } from "react-bootstrap";
+
 import api from "../../../services/api";
 import Loading from "../../Loading/Loading";
 import Erro from "../../Erro/Erro";
+import icons from "../../../assets/svgs";
 import './Movie.scss'
 
 const API_KEY = 'bf74bdfa989ad758eb544fbbde7650e4';
@@ -11,6 +14,7 @@ const language = 'pt-BR';
 
 function Movie() {
   const [movie, setMovie] = useState(null);
+  const [actors, setActors] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const { id } = useParams();
 
@@ -26,7 +30,16 @@ function Movie() {
           }
         });
 
+        const responseCredits = await api.get(`movie/${id}/credits`, {
+          params: {
+            api_key: API_KEY,
+            language: language
+          }
+        });
+
         setMovie(response.data);
+        setActors(responseCredits.data.cast);
+
         setIsLoading(false);
       } catch(e) {
         console.error(e);
@@ -58,13 +71,37 @@ function Movie() {
   
   const budget = movie.budget.toLocaleString('pt-BR');
 
+  const voteAverage = movie.vote_average.toFixed(1);
+
+  const renderStars = (rating) => {
+    const filledStars = Math.floor(rating);
+    const rest = rating - filledStars;
+
+    const stars = [];
+
+    for(let i = 0; i < filledStars; i++) {
+      stars.push(<i className="fa fa-star" key={i}></i>);
+    };
+
+    if(rest > 0) {
+      stars.push(<i className="fa fa-star-half" key={filledStars}></i>);
+    };
+
+    const emptyStars = 10 - (filledStars + (rest > 0 ? 1 : 0));
+    for(let i = 0; i < emptyStars; i++) {
+      stars.push(<i className="fa fa-star-o" key={filledStars + (rest > 0 ? 1 : 0) + i}></i>)
+    };
+
+    return stars;
+  }
+
   return (
     <div className="movie-details">
       <Container>
         <Row>
           <Col xs={12} md={4} className="movie-poster">
             <img 
-              src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+              src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
               alt={movie.title}
             >
             </img>
@@ -82,7 +119,27 @@ function Movie() {
                 {genre.name}
               </Badge>
             ))}
+            <div className="note-stars">
+              <p>Nota: {voteAverage}</p>
+              <p>{renderStars(voteAverage)}</p>
+            </div>
+            <Button variant="success" className="button-watchlist">
+              <i>{icons.bookmarkIcon}</i>
+              Adicionar a Watchlist
+            </Button>{' '}
           </Col>
+
+        </Row>
+        <Row className="actors-details">
+          <Col xs={12} md={12}>
+            <h2>ATORES:</h2>
+          </Col>
+            {actors.map((actor) => (
+            <Col xs={12} md={4}>
+              {/* <img src={`https://image.tmdb.org/t/p/w200/${actor.profile_path}`} alt={actor.name}></img> */}
+              <p key={actor.id}>{actor.name}</p>
+            </Col>
+            ))}
         </Row>
       </Container>
     </div>
