1190ac35bf008a4dc45fd800ace89ce32f0b5df7
anniasebold <anniasebold3.0@gmail.com>
2023-05-28 22:08:09 -0300

    css navbar ajustes

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,26 +1,42 @@
 import { Navbar, Nav, Container, Button } from "react-bootstrap";
 import Search from "../Search/Search";
 import './Header.scss'
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <>
-    <Navbar collapseOnSelect expand="lg" className="navbar" fixed="top">
-      <Container className="navbar">
-        <Nav className="nav-links" id="nav-logo">
-          <Navbar.Brand href="/">Movie Finder</Navbar.Brand>
+    <Navbar collapseOnSelect expand="lg" id="navbar" fixed="top">
+      <Container>
+        <Nav id="nav-logo">
+          <Link id="nav-links" to="/">
+            <Navbar.Brand>Movie Finder</Navbar.Brand>
+          </Link>
         </Nav>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Nav>
           <Search />
         </Nav>
         <Navbar.Collapse id="responsive-navbar-nav" className="custom-collapse">
-          <Nav className="nav-links justify-content-end flex-grow-1 pe-3">
-            <Nav.Link href="#watchlist" className="me-2">Watchlist</Nav.Link>
+          <Nav className="justify-content-end flex-grow-1 pe-3">
+            <Nav.Link id="logo-collapse" className="me-2">
+              <Link id="nav-links" to="/">
+                Movie Finder
+              </Link>
+            </Nav.Link>
+            <Nav.Link className="me-2">
+              <Link id="nav-links" to="/watchlist">
+                Watchlist
+              </Link>
+            </Nav.Link>
             <Nav.Link href="https://github.com/anniasebold/movie-finder" target="_blank" className="me-4">
               Github
             </Nav.Link>
-            <Button href="#login" variant="danger">Login</Button>
+            <Link id="nav-links" to="/login">
+              <Button variant="danger">
+                Login
+              </Button>
+            </Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
