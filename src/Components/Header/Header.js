8169be07c7fb082b193b87154d48216f09762a07
anniasebold <anniasebold3.0@gmail.com>
2023-05-30 15:12:19 -0300

    ajustes header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,6 +2,10 @@ import { Navbar, Nav, Container, Button } from "react-bootstrap";
 import Search from "../Search/Search";
 import './Header.scss'
 import { Link } from "react-router-dom";
+import logo from '../../assets/logo.png';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faFilm, faVideo } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function Header() {
   return (
@@ -10,7 +14,7 @@ function Header() {
       <Container>
         <Nav id="nav-logo">
           <Link id="nav-links" to="/">
-            <Navbar.Brand>Movie Finder</Navbar.Brand>
+            <img id="logo" src={logo} alt="logo Movie Finder" />
           </Link>
         </Nav>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
@@ -21,21 +25,21 @@ function Header() {
           <Nav className="justify-content-end flex-grow-1 pe-3">
             <Nav.Link id="logo-collapse" className="me-2">
               <Link id="nav-links" to="/">
+                <FontAwesomeIcon icon={faVideo} className="icon-video" />
                 Movie Finder
               </Link>
             </Nav.Link>
             <Nav.Link className="me-2">
               <Link id="nav-links" to="/watchlist">
+                <FontAwesomeIcon icon={faFilm} className="icon-watchlist" />
                 Watchlist
               </Link>
             </Nav.Link>
             <Nav.Link href="https://github.com/anniasebold/movie-finder" target="_blank" className="me-4">
-              Github
+              <FontAwesomeIcon icon={faGithub} className="icon-github" alt="icone github" />
             </Nav.Link>
             <Link id="nav-links" to="/login">
-              <Button variant="danger">
-                Login
-              </Button>
+              <Button variant="danger">Login</Button>
             </Link>
           </Nav>
         </Navbar.Collapse>
